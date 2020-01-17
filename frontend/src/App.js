558cce478337d8bf92d831ae5aa1b13b34c1af8a
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2020-01-17 15:23:12 -0300

    feat: Create devradar frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import api from './services/api';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      const data = response.data.map(dev => ({
+        ...dev,
+        techs: dev.techs.join(', ')
+      }));
+
+      setDevs(data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev({ github_username, techs, latitude, longitude }) {
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/DevForm/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+
+export default function DevForm({ onSubmit }) {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        setLatitude(coords.latitude);
+        setLongitude(coords.longitude);
+      },
+      error => console.log(error),
+      { timeout: 30000 }
+    );
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    await onSubmit({ github_username, techs, latitude, longitude });
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do Github</label>
+        <input
+          id="github_username"
+          name="github_username"
+          value={github_username}
+          onChange={e => setGithubUsername(e.target.value)}
+          required
+        />
+      </div>
+
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          id="techs"
+          name="techs"
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+          required
+        />
+      </div>
+
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            id="latitude"
+            type="number"
+            name="latitude"
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            id="longitude"
+            type="number"
+            name="longitude"
+            value={longitude}
+            onChange={e => setLongitude(e.target.value)}
+            required
+          />
+        </div>
+      </div>
+
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
